dbf25101b36a56905bdf3a1b79f722d475716b43
jdang00 <justindang02@gmail.com>
2025-07-12 21:17:55 -0500

    revert to theme

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -11,10 +11,10 @@
 				destructive:
 					'bg-destructive shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 text-white',
 				outline:
-					'bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 border',
-				secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
-				ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-				link: 'text-primary underline-offset-4 hover:underline'
+					"bg-background shadow-xs hover:bg-secondary hover:text-secondary-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 border",
+				secondary: "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+				ghost: "hover:bg-secondary hover:text-secondary-foreground dark:hover:bg-secondary/50",
+				link: "text-primary underline-offset-4 hover:underline"
 			},
 			size: {
 				default: 'h-9 px-4 py-2 has-[>svg]:px-3',
